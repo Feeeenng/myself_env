1016dea6e06a7da8542ca1e819da637a2d8c13cf
Feng <z332007851@163.com>
2017-10-15 15:35:54 +0800

    link

--- env_bash/init.sh
+++ env_bash/init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir -p /opt/python
+mkdir -p /opt/python3.6
 apt-get -y install libbz2-dev libgdbm-dev liblzma-dev libreadline-dev libsqlite3-dev libssl-dev tcl-dev tk-dev dpkg-dev
 wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
 tar xf Python-3.6.2.tgz
 cd Python-3.6.2
 ./configure --prefix=/opt/python3.6 --enable-optimizations
 make -j2 && make install
-source /etc/profile
+ln -s /opt/python3.6/bin/python3.6 /usr/bin/python3.6
+ln -s /opt/python3.6/bin/pip3.6 /usr/local/bin/pip3.6
