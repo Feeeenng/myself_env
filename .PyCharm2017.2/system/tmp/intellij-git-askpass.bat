35dff61b6ea0105bc1e00eb14a8c01465746b2e3
feng <z332007851@163.com>
2017-10-16 16:04:55 +0800

    add pycharm options

--- .PyCharm2017.2/system/tmp/intellij-git-askpass.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"D:/Program Files/JetBrains/PyCharm 2017.2.3/jre64/bin/java" -cp "D:/Program Files/JetBrains/PyCharm 2017.2.3/plugins/git4idea/lib/git4idea-rt.jar;D:/Program Files/JetBrains/PyCharm 2017.2.3/lib/xmlrpc-2.0.jar;D:/Program Files/JetBrains/PyCharm 2017.2.3/lib/commons-codec-1.9.jar;D:/Program Files/JetBrains/PyCharm 2017.2.3/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp %*
