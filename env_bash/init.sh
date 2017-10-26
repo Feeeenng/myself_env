65ec7ffa4fca0eedc23397d057ef0f20fceea2e6
feng <z332007851@163.com>
2017-10-26 15:14:41 +0800

    change encoding Liunx

--- env_bash/init.sh
+++ env_bash/init.sh
@@ -6,6 +6,6 @@ wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
 tar xf Python-3.6.2.tgz
 cd Python-3.6.2
 ./configure --prefix=/opt/python3.6 --enable-optimizations
-make -j2 && make install
-ln -s /opt/python3.6/bin/python3.6 /usr/bin/python3.6
+make -j4 && make install
+ln -s /opt/python3.6/bin/python3.6 /usr/bin/python3.
 ln -s /opt/python3.6/bin/pip3.6 /usr/local/bin/pip3.6
